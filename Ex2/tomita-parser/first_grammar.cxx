64b5e3ec6ac33d2d13d5dc902ba73a51ce3fb963
BlackSkullGTO <55648476+BlackSkullGTO@users.noreply.github.com>
2021-06-17 23:43:24 +0300

    Update first_grammar.cxx

--- Ex2/tomita-parser/first_grammar.cxx
+++ Ex2/tomita-parser/first_grammar.cxx
@@ -2,6 +2,4 @@
 
 Name -> Word<kwtype=celebrities> | Noun<kwtype=attractions>;
 
-Id -> AnyWord<wff=/[1-9][0-9][0-9][0-9][0-9][0-9][0-9]/>;
-
-Sentence -> Id interp (News.id::not_norm) AnyWord* Name interp (News.name);
+Sentence -> AnyWord* Name interp (Tomita_News.name) AnyWord*;
