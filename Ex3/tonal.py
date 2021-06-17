19cbdc99a0bd65882db662f80cb35e5342c4143b
BlackSkullGTO <55648476+BlackSkullGTO@users.noreply.github.com>
2021-06-17 23:50:17 +0300

    Update tonal.py

--- Ex3/tonal.py
+++ Ex3/tonal.py
@@ -1,142 +1,108 @@
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import stopwords
+from nltk.tag import pos_tag
+from nltk.tokenize import word_tokenize
+from nltk import FreqDist, classify, NaiveBayesClassifier
 from pymongo import MongoClient, ASCENDING, DESCENDING
-from pyspark.sql import SparkSession
-from pyspark.ml.feature import Tokenizer
-from pyspark.ml.feature import StopWordsRemover
-from pyspark.ml.feature import CountVectorizer
-from pyspark.ml.feature import IDF
-from pyspark.ml.feature import Word2Vec
-import re, string, datetime
+import re, string, random, csv
 
-def remove_punctuation(text):
+def remove_noise(tweet_tokens,stop_words=()):
 
-    """ 
-    Удаление пунктуации из текста
-    """ 
-    return text.translate(str.maketrans('', '', string.punctuation)).lower()
-    
-def remove_linebreaks(text): 
+	cleaned_tokens=[]
 
-    """ 
-    Удаление разрыва строк из текста
-    """ 
-    return text.strip()
-    
-def get_only_words(tokens):
+	for token, tag in pos_tag(tweet_tokens, lang='rus'):
+		token=re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|"\"(?:%[0-9a-fA-F][0-9a-fA-F]))+','',token)
+		token=re.sub("(@[A-Za-z0-9_]+)","",token)
+		if tag.startswith("NN"):
+			pos='n'
+		elif tag.startswith('VB'):
+			pos='v'
+		else:
+			pos='a'
+		lemmatizer=WordNetLemmatizer()
+		token=lemmatizer.lemmatize(token,pos)
+		if len(token)>0 and token not in string.punctuation and token.lower() not in stop_words: 
+			cleaned_tokens.append(token.lower())
+	return cleaned_tokens
 
-    """ 
-    Получение списка токенов, содержащих только слова
-    """ 
-    return list(filter(lambda x: re.match('[а-яА-Я]+', x), tokens))
+def get_all_words(cleaned_tokens_list):
+	for tokens in cleaned_tokens_list:
+		for token in tokens:
+			yield token
+
+def get_tweets_for_model(cleaned_tokens_list):
+	for tweet_tokens in cleaned_tokens_list:
+		yield dict([token,True]for token in tweet_tokens)
 
 client = MongoClient()
 database = client.news_database
-news = database.news
 sentences = database.sentences
 
-f = open('input.txt', 'w')
+stop_words=stopwords.words('russian')
+
+positive_tweet_tokens = []
+negative_tweet_tokens = []
+
+with open('positive2.csv',encoding='utf-8',newline='') as csvfile:
+   reader = csv.DictReader(csvfile, delimiter=';')
+   i = 0
+   for row in reader:
+       i+=1
+       if i == 20000:
+            break
+       tweet = row['tweet']
+       tokens = word_tokenize(tweet)
+       positive_tweet_tokens.append(tokens)
+
+with open('negative2.csv',encoding='utf-8',newline='') as csvfile:
+   reader = csv.DictReader(csvfile, delimiter=';')
+   i = 0
+   for row in reader:
+       i+=1
+       if i == 20000:
+            break
+       tweet = row['tweet']
+       tokens = word_tokenize(tweet)
+       negative_tweet_tokens.append(tokens)
+
+positive_cleaned_tokens_list = []
+negative_cleaned_tokens_list = []
+
+for tokens in positive_tweet_tokens:
+    positive_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
+for tokens in negative_tweet_tokens:
+    negative_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
+
+all_pos_words = get_all_words(positive_cleaned_tokens_list)
+
+freq_dist_pos=FreqDist(all_pos_words)
+print(freq_dist_pos.most_common(10))
+
+positive_tokens_for_model=get_tweets_for_model(positive_cleaned_tokens_list)
+negative_tokens_for_model=get_tweets_for_model(negative_cleaned_tokens_list)
+
+positive_dataset=[(tweet_dict,"Positive")for tweet_dict in positive_tokens_for_model]
+negative_dataset=[(tweet_dict,"negative")for tweet_dict in negative_tokens_for_model]
+
+dataset=positive_dataset+negative_dataset
+
+random.shuffle(dataset)
+
+train_data=dataset[:35000]
+test_data=dataset[35001:]
+
+classifier=NaiveBayesClassifier.train(train_data)
+print("Accuracy is:", classify.accuracy(classifier, test_data))
+print(classifier.show_most_informative_features(10))
+
+sentenceTexts = sentences.find({})
 i = 0
-for x in news.find( {} ):
+for sentenceText in sentenceTexts:
     i += 1
-    f.write(x['text']+'\n')
-    if i == 1000:
+    custom_tweet=sentenceText['text']
+    print(custom_tweet)
+    custom_tokens = remove_noise(word_tokenize(custom_tweet))
+    print(classifier.classify(dict([token,True]for token in custom_tokens)))
+    print('\n\n\n')
+    if i == 500:
         break
-f.close()
-
-spark = SparkSession.builder.appName("SimpleApplication").getOrCreate()
-
-# Построчная загрузка файла в RDD
-input_file = spark.sparkContext.textFile('input.txt')
-
-print('Подготовка данных 1')
-
-########################
-prepared = input_file.map(lambda x: (remove_punctuation(x))).map(lambda x: (remove_linebreaks(x)))
-prepared = prepared.map(lambda x: ([x]))
-
-prepared_df = prepared.toDF().selectExpr('_1 as text')
-#df = prepared.toDF()
-#prepared_df = df.selectExpr('_1 as text')
-########################
-
-print('Подготовка данных 2')
-
-# Разбить на токены
-tokenizer = Tokenizer(inputCol='text', outputCol='words')
-words = tokenizer.transform(prepared_df)
-words.show()
-
-print('Подготовка данных 3')
-
-# Удалить стоп-слова
-stop_words = StopWordsRemover.loadDefaultStopWords('russian')
-remover = StopWordsRemover(inputCol='words', outputCol='filtered', stopWords=stop_words)
-filtered = remover.transform(words)
-
-# Вывести стоп-слова для русского языка
-print(stop_words)
-
-# Вывести таблицу filtered
-filtered.show()
-
-# Вывести столбец таблицы words с токенами до удаления стоп-слов
-words.select('words').show(truncate=False, vertical=True)
-
-# Вывести столбец "filtered" таблицы filtered с токенами после удаления стоп-слов
-filtered.select('filtered').show(truncate=False, vertical=True)
-
-# Посчитать значения TF
-vectorizer = CountVectorizer(inputCol='filtered', outputCol='raw_features').fit(filtered)
-featurized_data = vectorizer.transform(filtered)
-featurized_data.cache()
-vocabulary = vectorizer.vocabulary
-
-# Вывести таблицу со значениями частоты встречаемости термов.
-featurized_data.show()
-
-# Вывести столбец "raw_features" таблицы featurized_data
-featurized_data.select('raw_features').show(truncate=False, vertical=True)
-
-# Вывести список термов в словаре
-print(vocabulary)
-
-
-# Посчитать значения DF
-idf = IDF(inputCol='raw_features', outputCol='features')
-idf_model = idf.fit(featurized_data)
-rescaled_data = idf_model.transform(featurized_data)
-
-# Вывести таблицу rescaled_data
-rescaled_data.show()
-
-# Вывести столбец "features" таблицы featurized_data
-rescaled_data.select('features').show(truncate=False, vertical=True)
-
-# Построить модель Word2Vec
-word2Vec = Word2Vec(vectorSize=3, minCount=0, inputCol='words', outputCol='result')
-model = word2Vec.fit(words)
-w2v_df = model.transform(words)
-w2v_df.show()
-
-f = open('/home/vagrant/Cursovaya/Ex2/names.txt', 'r')
-for line in f:
-    #Ввод слова
-    _words = line.replace("\n", "")
-    _words = _words.replace(" ", "_")
-    words = _words.split("_")
-    for word in words:
-    #Проверка на вхождение
-        intext=False
-        for list in rescaled_data.select('filtered').collect():
-            if list[0].count(word)!=0:intext=True
-    #Вывод
-        if intext:
-            print(_words)
-            synonyms = model.findSynonyms(word, 5)
-            synonyms.show()
-        else:
-            print(_words)
-            print("Synonims is not found")
-        print("\n\n\n\n\nEND")
-f.close()
-
-spark.stop()
